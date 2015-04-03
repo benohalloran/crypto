aefcf2df5e8ac3127d83d026f3134299e19d45d4
Ben O'Halloran <ben@ohalloran.io>
2015-04-03 19:45:04 -0400

    work on the things

---
+++ app/src/main/java/io/ohalloran/crypto/ConversationsActivity.java
@@ -0,0 +1,17 @@
+package io.ohalloran.crypto;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+//single conversation
+public class ConversationsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversations);
+    }
+
+
+
+}
--- app/src/main/java/io/ohalloran/crypto/MessagesActivity.java
+++ app/src/main/java/io/ohalloran/crypto/MessagesActivity.java
@@ -5,20 +5,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MessageList extends ActionBarActivity {
+//list of vonversations
+public class MessagesActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message_list);
+        setContentView(R.layout.activity_messages);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_message_list, menu);
+        getMenuInflater().inflate(R.menu.menu_conversation_acitivity, menu);
         return true;
     }
 
