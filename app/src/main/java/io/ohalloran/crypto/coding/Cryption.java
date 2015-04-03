c69c923bd5a7d060f042149c66baf284ecfdb72a
Ben O'Halloran <ben@ohalloran.io>
2015-04-03 19:54:26 -0400

    Skeletal encryption methods

---
+++ app/src/main/java/io/ohalloran/crypto/coding/Cryption.java
@@ -0,0 +1,16 @@
+package io.ohalloran.crypto.coding;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Ben on 4/3/2015.
+ */
+public class Cryption {
+    public static String decode(Bitmap map) {
+        return "DEFAULT";
+    }
+
+    public static Bitmap encode(String msg) {
+        return null;
+    }
+}
