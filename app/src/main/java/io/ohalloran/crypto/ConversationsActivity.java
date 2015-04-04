ceed4a1353ac1d409105703f136a3cbdd98c79a4
jbisnett <jacob.bisnett@gmail.com>
2015-04-03 23:25:08 -0400

    lots of stuff
    implemented RSA
    started encode image

--- app/src/main/java/io/ohalloran/crypto/ConversationsActivity.java
+++ app/src/main/java/io/ohalloran/crypto/ConversationsActivity.java
@@ -5,7 +5,12 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import io.ohalloran.crypto.coding.Cryption;
 
 //single conversation
@@ -16,7 +21,32 @@ public class ConversationsActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversations);
         Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.bird2);
-        Log.d("encryption", Cryption.pictureDecode(image));
+        //Log.d("encryption", Cryption.pictureDecode(image));
+        Bitmap image2 = Cryption.pictureEncode("",image);
+        Boolean b = image.sameAs(image2);
+        Log.d("encode test", "The images are equal?: " + b);
+
+
+    }
+    private void testRSA(){
+        String t = "this is a test";
+        PublicKey puk = null;
+        PrivateKey prk = null;
+
+        try{
+            KeyPair kp = Cryption.getKeyPair(1024);
+            puk = kp.getPublic();
+            prk = kp.getPrivate();
+            byte [] enc = Cryption.RSAEncrypt(t,puk);
+            Log.d("byte enc", new String(enc));
+            String dec = Cryption.RSADecrypt(enc,prk);
+            Log.d("byte dec", dec);
+
+
+        }
+        catch (Exception e){
+            Log.e("Key Pair Gen", "Error in Key Pair generation");
+        }
     }
 
 
