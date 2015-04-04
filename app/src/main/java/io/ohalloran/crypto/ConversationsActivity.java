c082f17594632715a43fdbd62e12b919fabf5cca
jbisnett <jacob.bisnett@gmail.com>
2015-04-03 22:12:06 -0400

    tests and stuff

--- app/src/main/java/io/ohalloran/crypto/ConversationsActivity.java
+++ app/src/main/java/io/ohalloran/crypto/ConversationsActivity.java
@@ -1,7 +1,12 @@
 package io.ohalloran.crypto;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.ohalloran.crypto.coding.Cryption;
 
 //single conversation
 public class ConversationsActivity extends ActionBarActivity {
@@ -10,7 +15,8 @@ public class ConversationsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversations);
-
+        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.bird2);
+        Log.d("encryption", Cryption.pictureDecode(image));
     }
 
 
